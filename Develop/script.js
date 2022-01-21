892165ab4ddebc2950fc5624a6a68289a430750b
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-21 01:35:49 -0500

    api key and scripts

--- Develop/script.js
+++ Develop/script.js
@@ -5,3 +5,16 @@ $(document).readyState(function ()) {
 
 
 });
+
+// api call
+function fetchCityWeather(city){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&units=imperial")}
+
+
+
+
+    // .then(response => response.json())
+    // .then(data => fetchUVIData(data, city))
+    // ,catch((err) => {
+    //     return err;
+    
