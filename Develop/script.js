8d96c15ebe53b5d826f0f8ab514ae1fe62ec1b82
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-21 17:23:24 -0500

    used 06 activities to write api call function

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,24 @@
-$(document).readyState(function ()) {
-    var searchTerm 
+
+// Event listenser search button code
 
 
 
 
-});
 
-// api call
+
+
+
+
+
+// api call for weather
 function fetchCityWeather(city){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&units=imperial")}
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&units=imperial")
 
+    .then(response =>    response.json())
+    .then(data => fetchUVIData(data,city))
+    .catch((err) => {
 
+        return err;
+    })
 
-
-    // .then(response => response.json())
-    // .then(data => fetchUVIData(data, city))
-    // ,catch((err) => {
-    //     return err;
-    
+}
