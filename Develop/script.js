aee731ddc315b8506588d97d43ed349a44aea4fd
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-25 01:20:54 -0500

    js code push

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,32 @@
 
+$(document).ready(function () {
+   
+    $("#search-button").on("click", function () {
+        var searchTerm = $("#search-value").val();
+        $("#search-value").val("");
+        weatherFunction(searchTerm);
+        weatherForecast(searchTerm);
+    });
+    
+    $("#search-button").keypress(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        
+        if (keycode === 13) {
+            weatherFunction(searchTerm);
+            weatherForecast(searchTerm);
+        }
+    });
+    var history = JSON.parse(localStorage.getItem("history")) || [];//retreive items from local storage
+    if (history.length > 0) {
+        weatherFunction(history[history.length - 1]);
+    }
+    for (var i = 0; i < history.length; i++) {// row for each element 
+        createRow(history[i]);
+    }
+    function createRow(text) {
+        var listItem = $("<li>").addClass("list-group-item").text(text);
+        $(".history").append(listItem);
+    }
+    
+    
+    
