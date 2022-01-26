08ef62cd2181d90fb640155b35886a9fdda25330
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-25 21:06:50 -0500

    api's and ajax added

--- Develop/script.js
+++ Develop/script.js
@@ -35,4 +35,59 @@ $(document).ready(function () {
                         weatherForecast($(this).text());
                     });
                 
+                    function weatherFunction(searchTerm) {
+                        $.ajax({
+                            type: "GET",
+                            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&units=imperial",
+                        }).then(function (data) {
+                            if (history.indexOf(searchTerm) === -1) {
+                                history.push(searchTerm);
+                                localStorage.setItem("history", JSON.stringify(history));
+                                createRow(searchTerm);
+                
+                                // code to clear out old content
+                            }
+                            $("#today").empty();
+                            var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+                            var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+                            var card = $("<div>").addClass("card");
+                            var cardBody = $("<div>").addClass("card-body");
+                            var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+                            var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+                            var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
+                            var lon = data.coord.lon;
+                            var lat = data.coord.lat;
+                          
+                            $.ajax({
+                                type: "GET",
+                                url: "https://api.openweathermap.org/data/2.5/uvi?appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&lat=" + lat + "&lon=" + lon,
+                            }).then(function (response) {
+                                console.log(response);
+                                var uvColor;
+                                var uvResponse = response.value;
+                                var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
+                                var btn = $("<span>").addClass("btn btn-sm").text(uvResponse);
+                                if (uvResponse < 3) {
+                                    btn.addClass("btn-success");
+                                } else if (uvResponse < 7) {
+                                    btn.addClass("btn-warning");
+                                } else {
+                                    btn.addClass("btn-danger");
+                                }
+                                cardBody.append(uvIndex);
+                                $("#today .card-body").append(uvIndex.append(btn));
+                            });
+                            
+                            
+                            // add to the page and merges
+                            title.append(img);
+                            cardBody.append(title, temp, humid, wind);
+                            card.append(cardBody);
+                            $("#today").append(card);
+                            console.log(data);
+                        });
+                    }
                     
+                });
+                
+    
