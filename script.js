2dc198f2a98d98b9afb7b65be6fd66523f395424
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-03-18 17:41:21 -0400

    added js and now rendors all days

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ function displayHistoryItem(cityName) {
   historyElement.appendChild(historyItemElement)
 }
 
+function displayForecastItem (oneCallData,day,cityName){
+    const iconUrl = `https://openweathermap.org/img/wn/${oneCallData.daily[day].weather[0].icon}.png`
+  const temperature = oneCallData.daily[day].temp.day
+  const humidity = oneCallData.daily[day].humidity
+  const windSpeed = oneCallData.daily[day].wind_speed
+  const uvIndex = oneCallData.daily[day].uvi
+  const date = new Date().toLocaleDateString()
+
+  document.querySelector(`#forecast${day+1}-city`).textContent = cityName
+  document.querySelector(`#forecast${day+1}-date`).textContent = date
+  document.querySelector(`#forecast${day+1}-icon`).src = iconUrl
+  document.querySelector(`#forecast${day+1}-temperature`).textContent = temperature
+  document.querySelector(`#forecast${day+1}-humidity`).textContent = humidity
+  document.querySelector(`#forecast${day+1}-windspeed`).textContent = windSpeed
+  document.querySelector(`#forecast${day+1}-uv`).textContent = uvIndex
+}
+
+
 async function getWeatherInfo(cityName) {
   const weatherUrl = getWeatherUrl(cityName)
   const weatherResponse = await fetch(weatherUrl)
@@ -39,5 +57,9 @@ async function getWeatherInfo(cityName) {
   document.querySelector('#today-humidity').textContent = humidity
   document.querySelector('#today-windspeed').textContent = windSpeed
   document.querySelector('#today-uv').textContent = uvIndex
+
+  for(let i = 0; i < 5;i++){
+    displayForecastItem(oneCallData,i,cityName)
+  }
 }
 getWeatherInfo("San Diego")
