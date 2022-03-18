9eed0b9ead747854240f88a3c77128e1b9981820
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-03-18 16:15:55 -0400

    starting new JS for api call and city with 5 day

--- script.js
+++ script.js
@@ -1,93 +1,24 @@
+const apiKey="d80c128733419c6552a8e47781c43818"
 
-$(document).ready(function () {
-   
-    $("#search-button").on("click", function () {
-        var searchTerm = $("#search-value").val();
-        $("#search-value").val("");
-        weatherFunction(searchTerm);
-        weatherForecast(searchTerm);
-    });
+function getOneCallUrl(lon,lat){
+
+    return `https://api.openweathermap.org/data/2.5/onecall?lon=${lon}&lat=${lat}&appid=${apiKey}&exclude=minutely,hourly,alerts`
+
+
+
+}
+
+
+
+function displayHistoryItem(cityName){
+    const historyElement=document.querySelector(".history")
+    const historyItemElement=document.createElement("li")
+    historyItemElement.textContent=(cityName)
+    historyElement.appendChild(historyItemElement)
+}
+
+function getWeatherInfo(cityName){
+
+
     
-    $("#search-button").keypress(function (event) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        
-        if (keycode === 13) {
-            weatherFunction(searchTerm);
-            weatherForecast(searchTerm);
-        }
-    });
-    var history = JSON.parse(localStorage.getItem("history")) || [];//retreive items from local storage
-    if (history.length > 0) {
-        weatherFunction(history[history.length - 1]);
-    }
-    for (var i = 0; i < history.length; i++) {// row for each element 
-        createRow(history[i]);
-    }
-    function createRow(text) {
-        var listItem = $("<li>").addClass("list-group-item").text(text);
-        $(".history").append(listItem);
-    }
-    
-    
-                    // listener
-                    $(".history").on("click", "li", function () {
-                        weatherFunction($(this).text());
-                        weatherForecast($(this).text());
-                    });
-                
-                    function weatherFunction(searchTerm) {
-                        $.ajax({
-                            type: "GET",
-                            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&units=imperial",
-                        }).then(function (data) {
-                            if (history.indexOf(searchTerm) === -1) {
-                                history.push(searchTerm);
-                                localStorage.setItem("history", JSON.stringify(history));
-                                createRow(searchTerm);
-                
-                                // code to clear out old content
-                            }
-                            $("#today").empty();
-                            var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
-                            var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-                            var card = $("<div>").addClass("card");
-                            var cardBody = $("<div>").addClass("card-body");
-                            var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
-                            var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
-                            var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
-                            var lon = data.coord.lon;
-                            var lat = data.coord.lat;
-                          
-                            $.ajax({
-                                type: "GET",
-                                url: "https://api.openweathermap.org/data/2.5/uvi?appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&lat=" + lat + "&lon=" + lon,
-                            }).then(function (response) {
-                                console.log(response);
-                                var uvColor;
-                                var uvResponse = response.value;
-                                var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
-                                var btn = $("<span>").addClass("btn btn-sm").text(uvResponse);
-                                if (uvResponse < 3) {
-                                    btn.addClass("btn-success");
-                                } else if (uvResponse < 7) {
-                                    btn.addClass("btn-warning");
-                                } else {
-                                    btn.addClass("btn-danger");
-                                }
-                                cardBody.append(uvIndex);
-                                $("#today .card-body").append(uvIndex.append(btn));
-                            });
-                            
-                            
-                            // add to the page and merges
-                            title.append(img);
-                            cardBody.append(title, temp, humid, wind);
-                            card.append(cardBody);
-                            $("#today").append(card);
-                            console.log(data);
-                        });
-                    }
-                    
-                });
-                
-    
+}
