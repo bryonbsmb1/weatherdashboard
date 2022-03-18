280d14ec3ba7a1ad7df987d6a58d24dcf505882e
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-03-18 16:33:49 -0400

    added logic to get to the one call response

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const apiKey="d80c128733419c6552a8e47781c43818"
+const apiKey = 'd80c128733419c6552a8e47781c43818'
 
-function getOneCallUrl(lon,lat){
-
-    return `https://api.openweathermap.org/data/2.5/onecall?lon=${lon}&lat=${lat}&appid=${apiKey}&exclude=minutely,hourly,alerts`
-
-
-
+function getOneCallUrl(lon, lat) {
+  return `https://api.openweathermap.org/data/2.5/onecall?lon=${lon}&lat=${lat}&appid=${apiKey}&exclude=minutely,hourly,alerts`
+}
+function getWeatherUrl(cityName) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
 }
 
-
-
-function displayHistoryItem(cityName){
-    const historyElement=document.querySelector(".history")
-    const historyItemElement=document.createElement("li")
-    historyItemElement.textContent=(cityName)
-    historyElement.appendChild(historyItemElement)
+function displayHistoryItem(cityName) {
+  const historyElement = document.querySelector('.history')
+  const historyItemElement = document.createElement('li')
+  historyItemElement.textContent = cityName
+  historyElement.appendChild(historyItemElement)
 }
 
-function getWeatherInfo(cityName){
+async function getWeatherInfo(cityName) {
+  const weatherUrl = getWeatherUrl(cityName)
+  const weatherResponse = await fetch(weatherUrl)
+  const weatherData = await weatherResponse.json()
+  const lon = weatherData.coord.lon
+  const lat = weatherData.coord.lat
 
-
-    
+  const oneCallUrl = getOneCallUrl(lon, lat)
+  const oneCallResponse = await fetch(oneCallUrl)
+  const oneCallData = await oneCallResponse.json()
 }
